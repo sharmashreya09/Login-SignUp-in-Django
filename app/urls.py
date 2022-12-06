25a8863806abd6f2e5a827b902b14994f540c0ee
sharmashreya09 <sharmashreya092000@gmail.com>
2022-12-06 14:47:07 +0530

    my app first commit

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
 
     path('',Home,name="home"),
     path('signup/',SignUp.as_view(),name="Signup"),
-    path('login/',Login.as_view(),name="login")
+    path('login/',Login.as_view(),name="login"),
+    path('logout/',LogoutView.as_view(next_page='/login/'),name="logout")
 ]
